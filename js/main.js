e8d8f335e112c6f09acabd54ed9660039103a5d9
ckwlswn <63777145+ckwlswn@users.noreply.github.com>
2020-06-23 21:28:40 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+     // 비주얼 슬라이드
+     $('#visual .slider').bxSlider({
+        // 옵션
+        auto: true, // 자동재생(true, false)
+        mode: "fade", // 효과종류(fade, vertical, horizontal)
+    })
+
+    // 비디오
+    // $('#video-info .info-container').bxSlider();
+});
